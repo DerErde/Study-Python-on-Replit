784db39d6b28fdb584d2d15a809ad453c092b7f6
YourDot <50391632+DerErde@users.noreply.github.com>
2022-03-31 08:32:33 +0000

    choosing what fortune to show (random.randint() and "if")

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-# picking random numbers
-
+# choosing what fortune to show (random.randint() and "if")
 import random
 
-# print(random.randint(1, 10))
+fortune_number = random.randint(1, 3)
 
-# print(random.random()) # generates a random number 0.237847834
+fortune_text = ''
 
-# print(random.randint(-100, 999))
-
-answer = random.randint(1, 3)
-
-if answer == 1:
-  print("Yes")
-if answer == 2:
-  print("No")
-if answer == 3:
-  print("Maybe")
+if fortune_number == 1:
+  fortune_text = 'You will have a great day!'
+if fortune_number == 2:
+  fortune_text = 'Today will be tough...but worth it.'
+if fortune_number == 3:
+  fortune_text = 'You will get married this year!'
+  
+lucky_number = random.randint(1, 100)
+print(f'{fortune_text} Yout Lucky Nomber is: {lucky_number}')
