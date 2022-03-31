613e7a332d53745ffebefaf2e05e639e54c7b24f
YourDot <50391632+DerErde@users.noreply.github.com>
2022-03-31 08:53:20 +0000

    List, Loops, and Dictionaries. First - 'List and "append, insert, delete"'

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# List, Loops, and Dictionaries. First - 'List'
+# List, Loops, and Dictionaries. First - 'List and "append, insert, delete"'
 
 fav_movies = ["Sandlot", "The Lego movie", "The Red"]
 
-print(fav_movies[0])
+print(len(fav_movies)) # returns the number of
 
-fav_numbers = [4, 9, 33]
+fav_movies.append("Iron Man") # add new film in the list
 
-print(fav_numbers[0])
+print(len(fav_movies))
+
+print(fav_movies)
+
+fav_movies.insert(1, "Batman") # add new film in position 1
+
+print(fav_movies)
+
+del(fav_movies[2]) # delite film №3
