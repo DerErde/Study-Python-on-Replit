9d930f9b5be4ac7929007bcd4bd0352619cdfada
YourDot <50391632+DerErde@users.noreply.github.com>
2022-04-11 09:24:37 +0000

    Conditional operator: Transitivity

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 # choice between two values
 
+num1 = int(input())
+num2 = int(input())
+
+if num1 < num2:
+    print(num1, 'меньше чем', num2)
+if num1 > num2:
+    print(num1, 'больше чем', num2)
+if num1 == num2:                      # используем двойное равенство
+    print(num1, 'равно', num2)
+if num1 != num2:
+    print(num1, 'не равно', num2)
+
+
+"""
+if a != b != c:
+    print('числа не равны')
+else:
+    print('числа равны')
+"""
+
+"""
 answer = input('Какой язык программирования мы изучаем?')
 if answer == 'Python':
     print('Верно! Мы ботаем Python =)')
     print('Python - отличный язык!')
-  
+else:
+    print('Не совсем так!')
+"""
