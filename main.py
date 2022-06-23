5ec7b83ae928eb3b1a257e843d773aede189db7c
YourDot <50391632+DerErde@users.noreply.github.com>
2022-06-23 07:28:26 +0000

    Calculate any digit of a number.

--- main.py
+++ main.py
@@ -1,3 +1,122 @@
+# Чтение кода - это и есть основа программирования!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+# логические операторы: and, or, not
+"""
+
+"""
+# Вычисляем любую цифру числа.
+n = 1234        # четырехзначное число
+p = 1000        #         1000
+
+print(n // p)   # 1
+n = n % p       #    234
+p = p // 10     #         100
+
+print(n // p)   # 2
+n = n % p       #    34
+p = p // 10     #         10
+
+print(n // p)   # 3
+n = n % p       #    4
+p = p // 10     #         1
+
+print(n // p)   # 4
+
+# Разбор
+num = int(input())            # берем 5-ти значимое число к примеру "45826"
+digit1 = num // 10 ** 4            # получаем 1-е число "4"
+digit2 = (num // 10 ** 3) % 10     # получаем 2-е число "5"
+digit3 = (num // 10 ** 2) % 10     # получаем 3-е число "8"
+digit4 = (num // 10) % 10          # получаем 4-е число "2"
+digit5 = num % 10                  # получаем 5-е число "6"
+print(digit1, digit2, digit3, digit4, digit5,  sep='_')
+"""
+
+
+"""
+age = int(input('How old are you?'))
+if not age < 12:
+  print('Access is allowed.')
+else:
+  print('Access denied.')
+"""
+  
+"""
+age = int(input('How old are you?'))
+grade = int(input('which class are you in?'))
+city = input('What city do you live in?')
+if age >= 12 and grade >= 7 and (city == 'Moskov' or city == 'Saint-Petersberg'):
+  print('Access is allowed.')
+else:
+  print('Access denied.')
+"""
+
+"""
+age = int(input('Сколько вам лет?: '))
+grade = int(input('В каком классе вы учитесь?: '))
+city = input('В каком городе вы живете?: ')
+if age >= 12 and grade >= 7 and city == 'Москва':
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+"""
+"""
+city = input('В каком городе вы живете?: ')
+if city == 'Москва' or city == 'Санкт-Петербург' or city == 'Екатеринбург':
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+
+"""
+
+
+
+"""
+age = int(input('Сколько вам лет?: '))
+grade = int(input('В каком классе вы учитесь?: '))
+if age >= 12 and grade >= 7:
+    print('Доступ разрешен.')
+else:
+    print('Доступ запрещен.')
+"""
+
+
+"""
+
 # choice between two values
 # Write a program that reads one line
 
@@ -140,3 +259,4 @@ if c > 0:
     d = d + c
 print(d)
   
+"""
